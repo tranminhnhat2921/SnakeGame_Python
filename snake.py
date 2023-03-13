0d2be7fb100d4da76fd415df246dab9be38b4424
TMN <tranminhnhat2921@gmail.com>
2023-03-13 23:11:08 +0700

    Add color code

--- snake.py
+++ snake.py
@@ -1,15 +1,23 @@
 #import lib
 import pygame, random, time, sys
+pygame.init()
 
 #Load image
 m = 20
 imageBody = pygame.transform.scale(pygame.image.load('body.jpg'),(m,m))
 imageHead = pygame.transform.scale(pygame.image.load('head.jpg'),(m,m))
-imageFood = pygame.transform.scale(pygame.image.load('covid.jpg'),(m,m))
+imageFood = pygame.transform.scale(pygame.image.load('covid.png'),(m,m))
+
 #Create screen
-pygame.init()
 gameSuface = pygame.display.set_mode((735,475))
 pygame.display.set_caption('Snake game')
+
+#Color
+red = pygame.color(255,0,0)
+blue = pygame.color(65,105,255)
+black = pygame.color(255,0,0)
+white = pygame.color(255,255,255)
+gray = pygame.color(128,128,128)
 
 #Main loop
 while True:
