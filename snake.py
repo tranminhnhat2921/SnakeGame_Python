c1460dcee9c83bdb33e982834c727aa23c505218
TMN <tranminhnhat2921@gmail.com>
2023-03-13 22:26:19 +0700

    Add image for game

--- snake.py
+++ snake.py
@@ -1,6 +1,11 @@
 #import lib
 import pygame, random, time, sys
 
+#Load image
+m = 20
+imageBody = pygame.transform.scale(pygame.image.load('body.jpg'),(m,m))
+imageHead = pygame.transform.scale(pygame.image.load('head.jpg'),(m,m))
+imageFood = pygame.transform.scale(pygame.image.load('covid.jpg'),(m,m))
 #Create screen
 pygame.init()
 gameSuface = pygame.display.set_mode((735,475))
