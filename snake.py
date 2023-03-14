c7d34fb4e70b5117f9d9f5fb59e2c53b26400b49
TMN <tranminhnhat2921@gmail.com>
2023-03-14 18:25:58 +0700

    Add comon variable

--- snake.py
+++ snake.py
@@ -12,6 +12,19 @@ imageFood = pygame.transform.scale(pygame.image.load('covid.png'),(m,m))
 gameSuface = pygame.display.set_mode((735,475))
 pygame.display.set_caption('Snake game')
 
+#Variable
+snakePos = [100,60]
+snakeBody = [[100,60],[80,60],[60,60]]
+foodX = random.randrange(1,71)
+foodY = random.randrange(1,45)
+if foodX % 2 != 1 : foodX += 1
+if foodY % 2 != 1 : foodY += 1
+foodPos = [foodX*10, foodY*10]
+foodFlag = True
+direction = 'RIGHT'
+changeTo = direction
+score = 0
+
 #Color
 red = pygame.color(255,0,0)
 blue = pygame.color(65,105,255)
